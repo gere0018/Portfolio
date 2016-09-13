8df23dc403d5202a3106f68ca3e4443be00e06d4
Nehmat <gere0018@algonquinlive.com>
2016-09-12 21:48:11 -0400

    Clean up
    removed bower and its related files, added git ignore, added more
    content to portfolio

--- bower_components/AngularJS/test/e2e/fixtures/angular-already-loaded/script.js
+++
@@ -1,5 +0,0 @@
-angular.
-  module('test', []).
-  controller('TestController', function($scope) {
-    $scope.text = 'Hello, world!';
-  });
--- bower_components/AngularJS/test/e2e/tests/angular-already-loaded.spec.js
+++
@@ -1,9 +0,0 @@
-describe('App where angular is loaded more than once', function() {
-  beforeEach(function() {
-    loadFixture('angular-already-loaded');
-  });
-
-  it('should have the interpolated text', function() {
-    expect(element(by.binding('text')).getText()).toBe('Hello, world!');
-  });
-});
