0a5967ee13cc5c276e7363ecf3dd9e869d50c6b8
Nehmat <nehmat.gereige@gmail.com>
2022-09-13 13:25:44 -0400

    Design system reviewed content

--- js/modal-component.js
+++ js/modal-component.js
@@ -5,15 +5,19 @@ const e = React.createElement;
 const LicenseManagementModal = (props) => {
     return (
         <div className="col-md-8 col-md-offset-2">
-            <h2>License Management - Case Study 
-                <br/>A SAAS platform to show office 365 admins the full picture
-            </h2>
+            <h2>License Management - Case Study </h2>
+            <h4>A SAAS platform to show office 365 admins the full picture</h4>
             <div className="h-50"></div>
+             <div>
+
+             </div>
+            
+            
             <h4>Project overview</h4>
             <p>This project was a new module that I designed for Quest On Demand from Inception to implementation. We created the MVP in 2019 and added more details and features in the following years.
                 License management is a software as a service tool designed for Office 365 admins. Its main purpose is to give Admins full exposure and control of their various License pools, their cost,
                 use and availability. We wanted to show admins the information they needed that wasn’t readily accessible in the Microsoft admin centre in order to help them save costs and make informed
-                decisions.As the sole designer for this module I did the following:
+                decisions. As the sole designer for this module I did the following:
             </p>
             <ul>
                 <li>Conducted live user interviews to better understand the users’ needs and frustrations</li>
@@ -48,15 +52,15 @@ const LicenseManagementModal = (props) => {
 
 const DesignSystemModal = () => (
     <div className="col-md-8 col-md-offset-2">
-        <h2>Creating a UX team and a Design system
-            <br/> Case Study - Quest On Demand</h2>
+        <h2>Design Team &amp; Design system</h2>
+        <h4> Case Study - Quest On Demand</h4>
         <div className="h-50"></div>
-        <p>When I first joined Quest On Demand, it felt like I was the only UX designer. It took some digging to find out that we had some wonderful designers working 
+        <p>When I first joined Quest On Demand, I was the only UX designer on the team. It took some digging to find out that we had some wonderful designers working 
             quietly and independently around the world. After reviewing the product that I was hired to enhance, the biggest problem that stood out for me is inconsistency. 
             The modules and features in On Demand looked and behaved in a disconnected manner as if they didn’t belong together and this was a simple reflection of the design team. 
             People were disconnected, they designed features required for their modules without being aware of the big picture of the product. My mission therefore become clear, I had
-            to connect the people, bring a common vision and create a design team that worked together, and eventually create a design system of reusable components to bring consistency 
-            across all modules. This mission was accomplished within the first year of my joining Quest. I needed to advocate to senior management 
+            to connect the people as a design team and eventually create a design system of reusable components to bring consistency 
+            across all modules. This mission was successfully accomplished within the first year of my joining Quest.
         </p>
         <h4>It's all about the people</h4>
         <br /> <img src="img/portfolio/ds-team.jpg" alt="On demand UX/UI conference" className="img-responsive" />
@@ -72,16 +76,16 @@ const DesignSystemModal = () => (
             Team members from US, China and Russia were flown to our office in Kanata/Canada on October 2019. The conference resulted in deeper connections, setting goals for the year ahead and
             great collaboration between product managers, designers and developers.</p>
         <h4>Birth of a design system</h4>
-        <p>At the time, a design system was still a novice concept mainly used in larger companies. Our team had a style guide that didn’t extend to the design software.Our problem was lack of 
+        <p>At the time, a design system was still a novice concept mainly used in larger companies. Our team had a style guide that didn’t extend to the design software. Our problem was lack of 
             consistency across modules in the same product. Creating a design system that allowed designers to use the same components in different modules was a sure way to ensure design consistency
             and eventually product consistency across features and modules.
         </p>
-        <h4>Tools: Sketch & Abstract</h4>
+        <h4>Tools: Sketch &amp; Abstract</h4>
         <p>To start creating a design system, we first had to adopt the same design tools to work collaboratively. We started using Sketch with Abstract as a versioning tool for our files. It took 
             some convincing to show creative designers the value of such a logical process. Having a developer background, versioning made so much sense to me but it wasn’t a given for all. However, 
-            we successfully adopted Abstract and slowly found the joy in collaboration and branches from master that didn’t disrupt a team mates efforts. 
+            we successfully adopted Abstract and slowly found the joy in collaboration and branches from master that didn’t disrupt a team-mates efforts. 
         </p>
-        <h4>The research</h4>
+        <h4>Research</h4>
         <p>We needed to define the content of our design system. We conducted elaborate market and community research to learn from existing design systems and identify the structure that was most 
             suitable for our team. Since our Dev team used Angular/Material as the base of their code, we used google material design as a base for our design components.
         </p>
@@ -90,16 +94,19 @@ const DesignSystemModal = () => (
             whole team and enhanced in iterations. In parallel our individual features and modules started adopting the common component library.
         </p>
         <h4>A success story</h4>
-        <p>Our product started as puzzle pieces that did not fit but Today On Demand is a whole integrated software.This success was led by Inspired designers across the world who strongly promoted
+        <p>Our product started as puzzle pieces that did not fit but Today On Demand is a whole integrated software.This success was led by inspired designers across continents who strongly promoted
             a shared design system. Our individual teams started implementing and creating these components. I personally worked with the core Dev team to create the foundation shared component 
-            library that other teams inherited to implement the module’s designs.
+            library that other teams inherited to implement their designs.
         </p>
+        <br /> <img src="img/portfolio/ds-cover.jpg" alt="On demand design system" className="img-responsive" />
+        <br />
         <h4>Doing it all over again with Figma</h4>
         <p>A few years down the line, Quest’s structure and teams changed drastically and so did the world after a sadly harsh pandemic. I found myself once again with new team members that required
             a change of tools. Figma became the most popular tool on the market for being independent of device type and combining multiple functionalities. Without hesitation, I adapted the change
-            and led a new team to recreate our design system and update different components in the process. Alas “Change”, the only permanence in world of impermanence!
+            and led a new team to recreate our design system and update different components in the process. Recently, we successfully implemented "StoryBook" as a tool for developers to mirror and document the design system components.
+            <br/><i>Alas “Change”, the only permanent in world of impermanence!</i>
         </p>
-        <br />
+        <div className="h-50"></div>
     </div>
 );
 
